ecdf162e71e193620e14a2b9b0cf62ea882f02ec
Gregorys Giménez <gharys06@gmail.com>
2019-03-13 15:46:45 -0400

    orden del header, footer, y acerca de

--- header-responsive.php
+++ header-responsive.php
@@ -1,43 +1,57 @@
-	
 <div id="slide-out-widget-area-bg" class="slide-out-from-right dark"></div>
 
 <div id="slide-out-widget-area" class="slide-out-from-right" data-dropdown-func="separate-dropdown-parent-link" data-back-txt="Back">
-
 	<div class="inner-wrap">
 		<div class="inner" data-prepend-menu-mobile="true">
-
 			<a class="slide_out_area_close" href="#">
-				<span class="close-wrap"> <span class="close-line close-line1"></span> <span class="close-line close-line2"></span> </span>		  </a>
+				<span class="close-wrap"> 
+					<span class="close-line close-line1"></span>
+					<span class="close-line close-line2"></span> 
+				</span>
+			</a>
 
+			<div class="off-canvas-menu-container mobile-only">
 
-				<div class="off-canvas-menu-container mobile-only">
+				<ul class="menu">
+					<li <?=($activepageresponsive=="index")?'class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-6 menu-item-17"':'class ="menu-item menu-item-type-post_type menu-item-object-page menu-item-home page_item page-item-6 menu-item-17"'?>>
+						<a href="index.php">Inicio</a>
+					</li>
+					<li <?=($activepageresponsive=="about")?'class="menu-item menu-item-type-post_type menu-item-object-page menu-item-95 current-menu-item"':'class ="menu-item menu-item-type-post_type menu-item-object-page menu-item-95"'?>>
+						<a href="about.php">Acerca de Nosotros</a>
+					</li>
+					<li <?=($activepageresponsive=="products")?'class="menu-item menu-item-type-post_type menu-item-object-page menu-item-213 current-menu-item"':'class="menu-item menu-item-type-post_type menu-item-object-page menu-item-213"'?>>
+						<a href="products.php">Productos</a>
+					</li>
+					<li <?=($activepageresponsive=="contact-us")?'class="menu-item menu-item-type-post_type menu-item-object-page menu-item-216 current-menu-item"':'class="menu-item menu-item-type-post_type menu-item-object-page menu-item-216"'?>>
+						<a href="contact-us.php">Contactanos</a>
+					</li>
+				</ul>
 
-
-					<ul class="menu">
-						<li <?=($activepageresponsive=="index")?'class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-6 menu-item-17"':'class ="menu-item menu-item-type-post_type menu-item-object-page menu-item-home page_item page-item-6 menu-item-17"'?>><a href="index.php">Inicio</a></li>
-						<li <?=($activepageresponsive=="about")?'class="menu-item menu-item-type-post_type menu-item-object-page menu-item-95 current-menu-item"':'class ="menu-item menu-item-type-post_type menu-item-object-page menu-item-95"'?>><a href="about.php">Acerca de Nosotros</a></li>
-						<li <?=($activepageresponsive=="products")?'class="menu-item menu-item-type-post_type menu-item-object-page menu-item-213 current-menu-item"':'class="menu-item menu-item-type-post_type menu-item-object-page menu-item-213"'?>><a href="products.php">Productos</a></li>
-						<li <?=($activepageresponsive=="contact-us")?'class="menu-item menu-item-type-post_type menu-item-object-page menu-item-216 current-menu-item"':'class="menu-item menu-item-type-post_type menu-item-object-page menu-item-216"'?>><a href="contact-us.php">Contactanos</a></li>
-					</ul>
-
-					<ul class="menu secondary-header-items">					</ul>
-				</div>
-				<div id="text-2" class="widget widget_text"><h4>Wow look at this!</h4>			<div class="textwidget"><p>This is an optional, highly<br>
-				customizable off canvas area.</p>
+				<ul class="menu secondary-header-items"></ul>
 			</div>
-		</div><div id="text-4" class="widget widget_text">			<div class="textwidget"><h4>About Salient</h4>
-			<div class="textwidget">
-				<p>The Castle<br>
-					Unit 345<br>
-					2500 Castle Dr<br>
-				Manhattan, NY</p>
-				<p>T: <a href="..\salient-ascend\index.html#">+216 (0)40 3629 4753</a><br>
-					E: <a href="..\salient-ascend\index.html#">hello@themenectar.com</a></p>
+
+			<div id="text-2" class="widget widget_text">
+				<h4>Wow look at this!</h4>
+				<div class="textwidget">
+					<p>This is an optional, highly<br>
+					customizable off canvas area.</p>
+				</div>
+			</div>
+
+			<div id="text-4" class="widget widget_text">			<div class="textwidget"><h4>About Salient</h4>
+				<div class="textwidget">
+					<p>The Castle<br>
+						Unit 345<br>
+						2500 Castle Dr<br>
+					Manhattan, NY</p>
+					<p>T: <a href="..\salient-ascend\index.html#">+216 (0)40 3629 4753</a><br>
+						E: <a href="..\salient-ascend\index.html#">hello@themenectar.com</a></p>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-
-	<div class="bottom-meta-wrap"><ul class="off-canvas-social-links"></ul></div><!--/bottom-meta-wrap--></div> <!--/inner-wrap-->
-
+		<div class="bottom-meta-wrap">
+			<ul class="off-canvas-social-links"></ul>
+		</div>
+	</div> 
 </div>
--- header.php
+++ header.php
@@ -1,45 +1,60 @@
 <header id="top">
-				<div class="container">
-					<div class="row">
-						<div class="col span_3">
-							<a id="logo" href="index.php" data-supplied-ml="false">
-								<img class="stnd default-logo" alt="Salient Corporate Creative" src="img\logos\Logo_Laramed_Negro.png" srcset="img\logos\Logo_Laramed_Negro.png 1x, img\logos\Logo_Laramed_Negro.png 2x">
-								<img class="starting-logo default-logo" alt="Salient Corporate Creative" src="img\logos\Logo_Laramed_Blanco.png" srcset="img\logos\Logo_Laramed_Blanco.png 1x, img\logos\Logo_Laramed_Blanco.png 2x">
-								<img class="starting-logo dark-version default-logo" alt="Salient Corporate Creative" src="img\logos\Logo_Laramed_Negro.png" srcset="img\logos\Logo_Laramed_Negro.png 1x, img\logos\Logo_Laramed_Negro.png 2x"> 
-							</a>
+	<div class="container">
+		<div class="row">
+			<div class="col span_3">
+				<a id="logo" href="index.php" data-supplied-ml="false">
+					<img class="stnd default-logo" alt="Salient Corporate Creative" src="img\logos\Logo_Laramed_Negro.png" srcset="img\logos\Logo_Laramed_Negro.png 1x, img\logos\Logo_Laramed_Negro.png 2x">
+					<img class="starting-logo default-logo" alt="Salient Corporate Creative" src="img\logos\Logo_Laramed_Blanco.png" srcset="img\logos\Logo_Laramed_Blanco.png 1x, img\logos\Logo_Laramed_Blanco.png 2x">
+					<img class="starting-logo dark-version default-logo" alt="Salient Corporate Creative" src="img\logos\Logo_Laramed_Negro.png" srcset="img\logos\Logo_Laramed_Negro.png 1x, img\logos\Logo_Laramed_Negro.png 2x"> 
+				</a>
+			</div>
 
-						</div><!--/span_3-->
+			<div class="col span_9 col_last">
+				<div class="slide-out-widget-area-toggle mobile-icon slide-out-from-right" data-icon-animation="simple-transform">
+					<div>
+						<a href="#sidewidgetarea" class="closed">
+							<span> 
+								<i class="lines-button x2">
+									<i class="lines"></i>
+								</i>
+							</span> 
+						</a> 
+					</div> 
+				</div>
 
-						<div class="col span_9 col_last">
-							<div class="slide-out-widget-area-toggle mobile-icon slide-out-from-right" data-icon-animation="simple-transform">
-								<div> <a href="#sidewidgetarea" class="closed"> <span> <i class="lines-button x2"> <i class="lines"></i> </i> </span> </a> </div> 
+				<nav>
+					<ul class="sf-menu">	
+						<li id="menu-item-17"  <?=($activepage=="index")?'class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-6 current_page_item menu-item-17"':'class ="menu-item menu-item-type-post_type menu-item-object-page menu-item-home page_item page-item-6 menu-item-17"'?>>
+							<a href="index.php">Inicio</a>
+						</li>
+						<li id="menu-item-95"  <?=($activepage=="about")?'class="menu-item menu-item-type-post_type menu-item-object-page menu-item-95 current_page_item"':'class="menu-item menu-item-type-post_type menu-item-object-page menu-item-95"'?>>
+							<a href="about.php">Acerca de Nosotros</a>
+						</li>
+						<li id="menu-item-213" <?=($activepage=="products")?'class="menu-item menu-item-type-post_type menu-item-object-page menu-item-213 current_page_item"':'class="menu-item menu-item-type-post_type menu-item-object-page menu-item-213"'?>>
+							<a href="products.php">Productos</a>
+						</li>
+						<li id="menu-item-216" <?=($activepage=="contact-us")?'class="menu-item menu-item-type-post_type menu-item-object-page menu-item-216 current_page_item"':'class="menu-item menu-item-type-post_type menu-item-object-page menu-item-216"'?>>
+							<a href="contact-us.php">Contactanos</a>
+						</li>
+					</ul>
+
+					<ul class="buttons" data-user-set-ocm="1">
+						<li class="slide-out-widget-area-toggle" data-icon-animation="simple-transform">
+							<div>
+								<a href="#sidewidgetarea" class="closed">
+									<span> 
+										<i class="lines-button x2">
+											<i class="lines"></i>
+										</i> 
+									</span> 
+								</a>
 							</div>
+						</li>								
+					</ul>
+				</nav>
 
-
-							<nav>
-
-								<ul class="sf-menu">	
-									<li id="menu-item-17"  <?=($activepage=="index")?'class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-6 current_page_item menu-item-17"':'class ="menu-item menu-item-type-post_type menu-item-object-page menu-item-home page_item page-item-6 menu-item-17"'?>><a href="index.php">Inicio</a></li>
-									<li id="menu-item-95"  <?=($activepage=="about")?'class="menu-item menu-item-type-post_type menu-item-object-page menu-item-95 current_page_item"':'class="menu-item menu-item-type-post_type menu-item-object-page menu-item-95"'?>><a href="about.php">Acerca de Nosotros</a></li>
-									<li id="menu-item-213" <?=($activepage=="products")?'class="menu-item menu-item-type-post_type menu-item-object-page menu-item-213 current_page_item"':'class="menu-item menu-item-type-post_type menu-item-object-page menu-item-213"'?>><a href="products.php">Productos</a></li>
-									<li id="menu-item-216" <?=($activepage=="contact-us")?'class="menu-item menu-item-type-post_type menu-item-object-page menu-item-216 current_page_item"':'class="menu-item menu-item-type-post_type menu-item-object-page menu-item-216"'?>><a href="contact-us.php">Contactanos</a></li>
-								</ul>
-
-
-
-								<ul class="buttons" data-user-set-ocm="1">
-
-									<li class="slide-out-widget-area-toggle" data-icon-animation="simple-transform"><div> <a href="#sidewidgetarea" class="closed"> <span> <i class="lines-button x2"> <i class="lines"></i> </i> </span> </a> </div></li>								
-								</ul>
-
-
-							</nav>
-
-
-							<div class="logo-spacing"><img class="hidden-logo" alt="Salient Corporate 2" src="wp-content\uploads\2017\09\logo-robot.png"></div>					
-						</div><!--/span_9-->
-
-
-					</div><!--/row-->
-				</div><!--/container-->
-			</header>
+				<div class="logo-spacing"><img class="hidden-logo" alt="Salient Corporate 2" src="wp-content\uploads\2017\09\logo-robot.png"></div>		
+			</div>
+		</div>
+	</div>
+</header>
